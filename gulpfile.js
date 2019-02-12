34e11ed2bfd87771c6171d038391a3f0d9d3c585
Brad Harris <bradharris@Brads-MBP.hitronhub.home>
2019-02-12 12:16:11 -0800

    add modern gulp code

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-    return gulp.src('src/scss/app.scss')
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('app/css'));
-});
+// gulp.task('sass', function(){
+//     return gulp.src('src/scss/app.scss')
+//         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+//         .pipe(gulp.dest('app/css'));
+// });
 
-gulp.task('default');
+// gulp.task('default', gulp.series('sass'));
+
+
+// This is the new way to write a task in Gulp@4:
+const { src, dest, series } = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+ 
+function compileSass() {
+  return src('src/scss/app.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(dest('app/css'));
+}
+ 
+exports.compileSass = compileSass;
+exports.default = series(compileSass);
